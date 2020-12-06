34d7c6a39e6e909d640ce2ef4d7db64889d97823
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-12-06 18:09:32 +0300

    [+][Homework][Lesson4] Added empty asp.net mvc project

---
+++ Sources/Lesson4/Lesson4_Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson4_Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
