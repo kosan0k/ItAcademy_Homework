eabcb2666f108d68b798c7b7c3953d91d1fe2389
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-12-06 18:34:41 +0300

    [+] IT_AcademyHomework.Common.Validation project

---
+++ Sources/Common/IT_AcademyHomework.Common.Validation/IValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace IT_AcademyHomework.Common.Validation
+{
+    public interface IValidator<T>
+    {
+        ValidationResult Validate(T parameter);
+    }
+}
---
+++ Sources/Common/IT_AcademyHomework.Common.Validation/ValidationResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace IT_AcademyHomework.Common.Validation
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; }
+
+        public IReadOnlyCollection<string> Messages { get; }
+
+        public ValidationResult(bool isValid, IList<string> messages) 
+        {
+            IsValid = isValid;
+            Messages = new ReadOnlyCollection<string>(messages);
+        }
+    }
+}
