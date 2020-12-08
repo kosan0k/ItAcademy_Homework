729b95f9bcaf80b93d488ea85d4e47f518fc77e4
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-12-08 23:09:37 +0300

    [+] Custom action result

---
+++ Sources/Lesson4/Lesson4_Homework/Other/CustomActionResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lesson4_Homework.Other
+{
+    public class CustomActionResult : IActionResult
+    {
+        private const string _responseTitle = "<h2> Здесь должна была быть реклама Cyberpunk2077</h2>";        
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+            fullHtmlCode += "<title>Главная страница</title>";
+            fullHtmlCode += "<meta charset=utf-8 />";
+            fullHtmlCode += "</head> <body>";
+            fullHtmlCode += _responseTitle;
+            fullHtmlCode += "</body></html>";
+            await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+        }
+    }
+}
