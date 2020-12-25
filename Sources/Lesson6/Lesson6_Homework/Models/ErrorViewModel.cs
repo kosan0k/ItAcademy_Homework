f91490c7175b8597752b7c653cab9ae81a447230
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-12-25 18:18:29 +0300

    [+] 6 lesson homework project

---
+++ Sources/Lesson6/Lesson6_Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson6_Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
